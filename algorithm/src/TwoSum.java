f91b208722724e0dd0329f398155defcd4edfe37
DESKTOP-R2T16A2\admin <11111>
2020-12-17 18:21:33 +0800

    RichestCustomerWealth

--- algorithm/src/TwoSum.java
+++ algorithm/src/TwoSum.java
@@ -18,20 +18,20 @@ import java.util.Map;
 public class TwoSum {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{2, 7, 11, 15};
+        int[] numbs = new int[]{2, 7, 11, 15};
         int target = 9;
-        int[] result = twoSum(nums, target);
+        int[] result = twoSum(numbs, target);
         System.out.println(Arrays.toString(result));
     }
 
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] numbs, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int data = target - nums[i];
+        for (int i = 0; i < numbs.length; i++) {
+            int data = target - numbs[i];
             if (map.containsKey(data) && map.get(data) != i) {
                 return new int[]{map.get(data), i};
             }
-            map.put(nums[i], i);
+            map.put(numbs[i], i);
         }
         return null;
     }
